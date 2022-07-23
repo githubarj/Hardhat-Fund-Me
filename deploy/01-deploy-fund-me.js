7f054282b33e293737b5c3fec71d8fdd42ed8f54
githubarj <githuba@GitthubaRJ.localdomain>
2022-07-23 20:47:13 +0300

    deploy script working

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -12,8 +12,53 @@ calling of main */
 // module.exports = async (hre) => {
 //     const { getNamedAccounts, deployments } = hre //pull these exact variables from hre
 // }
+
+// const helperConfig = require("../helper-hardhat-config")
+// const networkConfig = helperConfig.networkConfig
+
+const {
+    networkConfig,
+    developmentChains,
+} = require("../helper-hardhat-config"); //easy way to pull just networkconfig from the helper file
+const { network } = require("hardhat");
+const { verify } = require("../utils/verify")
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
-   const { deploy, log } = deployments //pulling htse two functions out of deployment
-   const { deployer } = await getNamedAccounts()
-   const chainId = network.config.chaindId //we grab our chainid
-}
+    const { deploy, log } = deployments; //pulling these two functions out of deployment
+    const { deployer } = await getNamedAccounts(); //getting our deployer acount from the getNamed accounts function
+    const chainId = network.config.chainId; //we grab our chainid
+
+    //what happens when we want to change chain?
+    //use ifchain id is xx use y if chain id is y use x
+    //  const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]; //it will assign the usdpricefeed contract address according to chain
+    //when going for localhost or hardhat we want to use a mock
+    //so if the contract doesen't exists, we used a  minimal version for local testing
+
+    let ethUsdPriceFeedAddress; //declare using let so we can change it
+
+    if (developmentChains.includes(network.name)) {
+        //get the most recent deployment
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+        ethUsdPriceFeedAddress = ethUsdAggregator.address;
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    }
+
+    const args = [ethUsdPriceFeedAddress];
+    const fundMe = await deploy("FundMe", {
+        from: deployer, //who is running it
+        args: args, //put the price feed address
+        log: true, //prints a couple of information
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    //verification,checking if on a testnet
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
+          //VERIFY
+          await verify(fundMe.address, args);
+    }
+
+    log("----------------------------------------------");
+};
+
+module.exports.tags = ["all", "fundme"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,47 +5,43 @@ require("hardhat-gas-reporter");
 require("solidity-coverage");
 require("hardhat-deploy");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-    solidity: "0.8.8",
+    //adding multiple versions of solodity compiler
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    defaultNetwork: "hardhat", //specifying the default network, if you do not write it in, it will be hardhat
     networks: {
-        ropsten: {
-            url: process.env.ROPSTEN_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
         },
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
         currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
     etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY,
     },
-    namedAccounts: { //naimng accounts in the acounts array to better keep track of them
-      deployer:{
-        default: 0, //by default the accout at the 0th index will be deployer
-        //4: 1 //to set the default for particular network, chaindId: index
-      },
-      user: {
-        default: 1,
-      }
-    }
+    namedAccounts: {
+        //naimng accounts in the acounts array to better keep track of them
+        deployer: {
+            default: 0, //by default the accout at the 0th index will be deployer on the default network
+            //4: 1 //to set the default for particular network, chaindId: index
+        },
+        user: {
+            default: 1,
+        },
+    },
 };
